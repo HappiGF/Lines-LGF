8f5a81684d052b12e3fb73cf0f63c207fc70d79e
Lucas Guzman-Finn <lggufi@gmail.com>
2017-05-12 12:32:10 -0400

    sdgaewgr

---
+++ LGF_Lines/Assets/Scripts/Movement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class Movement : MonoBehaviour {
+
+	float circleX = 0;
+	float circleY = -0.5;
+	int radius = 45;
+	double speedScale;
+	public float speed;
+
+	void Start() {
+		speed = 5;
+		speedScale = (0.001*2*Math.PI)/speed;
+	}
+
+	void ResetPosition() {
+		transform.position (0, 0, 40);
+	}
+
+	void RotateClockwise() {
+		var angle = getTimer()*speedScale;
+		double x = circleX + Math.sin(angle)*radius;
+		double y = circleY + Math.cos(angle)*radius;
+		transform.position (x, 0, y);
+	}
+
+	void RotateCounterClockwise() {
+
+	}
+}
---
+++ LGF_Lines/Assets/Scripts/TouchControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchControl : MonoBehaviour {
+
+	private Vector2 touchOrigin = -Vector2.one;
+	public object player;
+
+	void Update () {
+		if (Input.touchCount > 0)
+		{
+			Touch myTouch = Input.touches[0];
+
+			if (myTouch.phase == TouchPhase.Began) {
+				touchOrigin = myTouch.position;
+			}
+
+			if (touchOrigin.x > 0) {
+
+			} else if (touchOrigin.x < 0) {
+
+			}
+				
+		}
+	}
+}
