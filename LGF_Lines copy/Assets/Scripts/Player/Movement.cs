fee766944fc3ca57735c914a04bfc543238ceb06
Lucas Guzman-Finn <lggufi@gmail.com>
2017-05-24 14:38:57 -0400

    android sdk why

---
+++ LGF_Lines copy/Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	public float speed;
+	public int radius;
+	private Vector2 center;
+	private float angle;
+	public float distance;
+	public float score;
+
+	void Start() {
+		center = transform.position;
+		distance = 0;
+		score = 0;
+	}
+
+	void Update() {
+		if (Input.GetKey ("d") /*|| Input.touchCount > 0*/) {
+			RotateClockwise ();
+		} else if (Input.GetKey ("a")) {
+			RotateCClockwise ();
+		}
+		if (Input.touchCount > 0) {
+			if (Input.GetTouch (Input.touches.Length - 1).position.x > 750) {
+				RotateClockwise ();
+			} else {
+				RotateCClockwise ();
+			}
+		}
+
+		if (distance >= 3 || distance <= -3) {
+			distance = 0;
+			score += 1;
+		}
+
+	}
+
+	void RotateClockwise () {
+		angle += speed * Time.deltaTime;
+
+		Vector2 offset = new Vector2 (Mathf.Sin(angle), Mathf.Cos(angle)) * radius;
+		transform.position = center + offset;
+		distance += Time.deltaTime;
+	}
+
+	void RotateCClockwise() {
+		angle += speed * Time.deltaTime * -1;
+
+		Vector2 offset = new Vector2 (Mathf.Sin(angle), Mathf.Cos(angle)) * radius;
+		transform.position = center + offset;
+		distance -= Time.deltaTime;
+	}
+		
+}
